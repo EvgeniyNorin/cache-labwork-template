dd566487a43e5528f08ff29ee7f19ead00db9006
Evgeniy Norin <enorin@avito.ru>
2025-06-22 18:17:26 +0300

    Move tests to a separate directory, use empty cache as a placeholder

--- cache/fabric.go
+++ cache/fabric.go
@@ -6,33 +6,33 @@ import "time"
 // TODO: Implement this function
 func NewFIFOCache[K comparable, V any](capacity int) Cache[K, V] {
 	// Students should implement this
-	panic("NewFIFOCache not implemented")
+	return &emptyCache[K, V]{}
 }
 
 // NewLRUCache creates a new LRU (Least Recently Used) cache
 // TODO: Implement this function
 func NewLRUCache[K comparable, V any](capacity int) Cache[K, V] {
 	// Students should implement this
-	panic("NewLRUCache not implemented")
+	return &emptyCache[K, V]{}
 }
 
 // NewLFUCache creates a new LFU (Least Frequently Used) cache
 // TODO: Implement this function
 func NewLFUCache[K comparable, V any](capacity int) Cache[K, V] {
 	// Students should implement this
-	panic("NewLFUCache not implemented")
+	return &emptyCache[K, V]{}
 }
 
 // NewTTLCache creates a new TTL (Time To Live) cache
 // TODO: Implement this function
 func NewTTLCache[K comparable, V any](capacity int, ttl time.Duration) Cache[K, V] {
 	// Students should implement this
-	panic("NewTTLCache not implemented")
+	return &emptyCache[K, V]{}
 }
 
 // NewARCCache creates a new ARC (Adaptive Replacement Cache)
 // TODO: Implement this function (Advanced task)
 func NewARCCache[K comparable, V any](capacity int) Cache[K, V] {
 	// Students should implement this
-	panic("NewARCCache not implemented")
+	return &emptyCache[K, V]{}
 } 
